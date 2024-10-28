b587038d269f995f977037ec52e4bf32c4912fe7
geek <elmainiaymane03@gmail.com>
2024-10-28 01:37:29 +0100

    setup spring boot project

---
+++ src/main/java/com/wora/state_of_dev/StateOfDevApplication.java
@@ -0,0 +1,13 @@
+package com.wora.state_of_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StateOfDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StateOfDevApplication.class, args);
+	}
+
+}
