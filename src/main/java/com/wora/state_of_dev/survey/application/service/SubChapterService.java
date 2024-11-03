6973bee6dc1314f7dda3fae84806ff6e34218359
geek <elmainiaymane03@gmail.com>
2024-11-03 14:05:25 +0100

    remove subchapter service and controller with its tests

--- src/main/java/com/wora/state_of_dev/survey/application/service/SubChapterService.java
+++
@@ -1,9 +0,0 @@
-package com.wora.state_of_dev.survey.application.service;
-
-import com.wora.state_of_dev.survey.application.dto.request.ChapterRequestDto;
-import com.wora.state_of_dev.survey.application.dto.response.SubChapterResponseDto;
-import com.wora.state_of_dev.survey.domain.valueObject.ChapterId;
-
-public interface SubChapterService {
-    SubChapterResponseDto create(ChapterId parentId, ChapterRequestDto dto);
-}
