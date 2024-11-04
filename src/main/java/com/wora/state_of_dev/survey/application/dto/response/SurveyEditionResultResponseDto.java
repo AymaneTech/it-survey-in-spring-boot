48337433fdc343cef4cbcd04af88df09b3ca4f4c
geek <elmainiaymane03@gmail.com>
2024-11-04 21:29:26 +0100

    udpate result dto by adding edition year

--- src/main/java/com/wora/state_of_dev/survey/application/dto/response/SurveyEditionResultResponseDto.java
+++ src/main/java/com/wora/state_of_dev/survey/application/dto/response/SurveyEditionResultResponseDto.java
@@ -2,11 +2,13 @@ package com.wora.state_of_dev.survey.application.dto.response;
 
 import com.wora.state_of_dev.survey.application.dto.embeddable.AnswerEmbeddableDto;
 
+import java.time.Year;
 import java.util.List;
 
 public record SurveyEditionResultResponseDto(Long id,
                                              String title,
                                              String description,
+                                             Year year,
                                              List<NestedChapter> chapters
 ) {
     public record NestedChapter(Long id,
