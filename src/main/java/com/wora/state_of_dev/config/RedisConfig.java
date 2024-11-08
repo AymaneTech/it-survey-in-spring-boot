832eb0e9ff6bdbda6e65ffc3825ea18226c866de
geek <elmainiaymane03@gmail.com>
2024-11-08 13:22:23 +0100

    add a redis configuration

---
+++ src/main/java/com/wora/state_of_dev/config/RedisConfig.java
@@ -0,0 +1,32 @@
+package com.wora.state_of_dev.config;
+
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+//@EnableCaching
+public class RedisConfig {
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(60))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    }
+
+    @Bean
+    public RedisCacheManagerBuilderCustomizer redisManagerBuilderCustomizer() {
+        return builder -> {
+            var tmp = RedisCacheConfiguration.defaultCacheConfig()
+                    .entryTtl(Duration.ofMinutes(30));
+            builder.withCacheConfiguration("state_of_dev", tmp);
+        };
+    }
+}
