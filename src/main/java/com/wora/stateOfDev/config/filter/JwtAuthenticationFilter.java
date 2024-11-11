686ede07d5c164a7278ad459be93ae2415112adf
geek <elmainiaymane03@gmail.com>
2024-11-11 10:41:29 +0100

    remove unused variable

--- src/main/java/com/wora/stateOfDev/config/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/wora/stateOfDev/config/filter/JwtAuthenticationFilter.java
@@ -1,25 +1,26 @@
 package com.wora.stateOfDev.config.filter;
 
-import io.swagger.v3.oas.models.OpenAPI;
+import java.io.IOException;
+import java.util.Optional;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
-import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final UserDetailsService userDetailsService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
         final Optional<String> token = parseJwt(request);
         System.out.println("my custom filter worked");
 
@@ -30,6 +31,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         System.out.println("found a token ");
         System.out.println(token);
+        filterChain.doFilter(request, response);
 
     }
 
